59480220c47bdb007eaa1b0a94222229e97856c3
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2018-03-09 11:35:11 -0700

    Adds oh my zshrc

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+brew install autojump
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+rm -f ~/.zshrc
+ln -s ./dev-machine-settings/zsh/themes ~/.oh-my-zsh/custom/themes
+ln -s ~/dev-machine-settings/zsh/zshrc ~/.zshrc
+
+echo 'done'
