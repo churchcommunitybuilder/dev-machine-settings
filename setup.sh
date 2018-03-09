c8eb9b099cca7ae46b27bd370d127e4c7e53718b
Corey Winkelmann <cwinkelmann@churchcommunitybuilder.com>
2018-03-09 13:29:10 -0700

    Removes autojump

--- setup.sh
+++ setup.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-brew install autojump
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 rm -f ~/.zshrc
 ln -s ~/dev-machine-settings/zsh/zshrc ~/.zshrc
-
-echo 'done'
